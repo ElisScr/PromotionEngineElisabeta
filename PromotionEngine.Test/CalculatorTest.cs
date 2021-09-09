3eef697374cc716fcc682aa06826f177c27b98f3
eliscriba <eliscriba@outlook.com>
2021-09-09 16:25:29 +0200

    First Calculator Test Scenario

---
+++ PromotionEngine.Test/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        Calculator calc = new Calculator();
+
+        [TestMethod]
+        public void CalculatorScenario1()
+        {
+            var skus = new List<char> { 'A', 'B', 'C' };
+            int result = calc.GetTotalPrice(skus);
+
+            Assert.AreEqual(100, result);
+        }
+
+
+
+    }
+}
--- PromotionEngine/Calculator.cs
+++ PromotionEngine/Calculator.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PromotionEngine
 {
     public class Calculator
-    {           
-        
+    {
+        public int GetTotalPrice(List<char> skus)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

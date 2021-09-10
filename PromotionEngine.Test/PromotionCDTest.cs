511e7e4e37e50c62f63903dc53dcf10c719bad79
eliscriba <eliscriba@outlook.com>
2021-09-10 20:14:53 +0200

    Initial test for Promotion C+D

---
+++ PromotionEngine.Test/PromotionCDTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Test
+{
+    [TestClass]
+    public class PromotionCDTest
+    {
+
+        [TestMethod]
+        public void Promotion3Test1()
+        {
+            IEnumerable<char> skus = new List<char> { 'C', 'D' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char>(), result.RemainingSkus);
+            Assert.AreEqual(30, result.PromotionPrice);
+        }
+    }
+}
---
+++ PromotionEngine/PromotionCD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class PromotionCD : IPromotion
+    {
+        public PromotionResult Apply(IEnumerable<char> skus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

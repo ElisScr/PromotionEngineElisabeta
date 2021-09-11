4c0fd47b42ccfa5a8446feec027e62e9334b6e4f
eliscriba <eliscriba@outlook.com>
2021-09-11 12:11:53 +0200

    Promotion 3 tests and implementation

--- PromotionEngine.Test/PromotionCDTest.cs
+++ PromotionEngine.Test/PromotionCDTest.cs
@@ -20,5 +20,60 @@ namespace PromotionEngine.Test
             CollectionAssert.AreEquivalent(new List<char>(), result.RemainingSkus);
             Assert.AreEqual(30, result.PromotionPrice);
         }
+        [TestMethod]
+        public void Promotion3Test2()
+        {
+            IEnumerable<char> skus = new List<char> { 'C', 'D','C','D' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char>(), result.RemainingSkus);
+            Assert.AreEqual(60, result.PromotionPrice);
+        }
+        [TestMethod]
+        public void Promotion3Test3()
+        {
+            IEnumerable<char> skus = new List<char> { 'C', 'C', 'D', 'C', 'D' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char> { 'C'}, result.RemainingSkus);
+            Assert.AreEqual(60, result.PromotionPrice);
+        }
+        [TestMethod]
+        public void Promotion3Test4()
+        {
+            IEnumerable<char> skus = new List<char> { 'C', 'D','A', 'B' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char> { 'A', 'B'}, result.RemainingSkus);
+            Assert.AreEqual(30, result.PromotionPrice);
+        }
+        [TestMethod]
+        public void Promotion3Test5()
+        {
+            IEnumerable<char> skus = new List<char> { 'A', 'B' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char> { 'A', 'B' }, result.RemainingSkus);
+            Assert.AreEqual(0, result.PromotionPrice);
+        }
+        [TestMethod]
+        public void Promotion3Test6()
+        {
+            IEnumerable<char> skus = new List<char> { 'A', 'B', 'C' };
+
+            IPromotion prom = new PromotionCD();
+            PromotionResult result = prom.Apply(skus);
+
+            CollectionAssert.AreEquivalent(new List<char> { 'A', 'B' , 'C'}, result.RemainingSkus);
+            Assert.AreEqual(0, result.PromotionPrice);
+        }
     }
 }
--- PromotionEngine/PromotionCD.cs
+++ PromotionEngine/PromotionCD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PromotionEngine
@@ -8,7 +9,26 @@ namespace PromotionEngine
     {
         public PromotionResult Apply(IEnumerable<char> skus)
         {
-            throw new NotImplementedException();
+            
+            var count = skus.GroupBy(item => item)
+                    .Select(item => new
+                    {
+                        Name = item.Key,
+                        Count = item.Count()
+                    })
+                    .ToList();
+            
+            int numberOfPromotionsCD =Math.Min(count.FirstOrDefault(c => c.Name == 'C')?.Count ?? 0,
+                count.FirstOrDefault(c => c.Name == 'D')?.Count ?? 0);
+            List<char> temp = Enumerable.Repeat('D', numberOfPromotionsCD)
+                .Concat(Enumerable.Repeat('C', numberOfPromotionsCD)).ToList();
+            
+            PromotionResult result = new PromotionResult
+            {
+                PromotionPrice = numberOfPromotionsCD * 30,
+                RemainingSkus = skus.Where(t => !temp.Remove(t)).ToList()
+            };
+            return result;
         }
     }
 }

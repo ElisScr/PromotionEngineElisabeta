f21daf00b01b718df85e2199e51903c5f16f10d0
eliscriba <eliscriba@outlook.com>
2021-09-12 18:55:34 +0200

    Added Console Application logic

--- PromotionEngine.ConsoleApp/Program.cs
+++ PromotionEngine.ConsoleApp/Program.cs
@@ -6,7 +6,22 @@ namespace PromotionEngine.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Promotion Engine");
+            Console.WriteLine("First Scenario");
+            Calculator calculator = new Calculator();
+            int totalPriceScenario1= calculator
+                .GetTotalPrice(new System.Collections.Generic.List<char> { 'A', 'B', 'C' });
+            Console.WriteLine("Total: " +totalPriceScenario1);
+
+            Console.WriteLine("Second Scenario");
+            int totalPriceScenario2 = calculator
+                .GetTotalPrice(new System.Collections.Generic.List<char> { 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'C' });
+            Console.WriteLine("Total: " + totalPriceScenario2);
+
+            Console.WriteLine("Third Scenario");
+            int totalPriceScenario3 = calculator
+                .GetTotalPrice(new System.Collections.Generic.List<char> { 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'C', 'D' });
+            Console.WriteLine("Total: " + totalPriceScenario3);
 
             Console.ReadKey();
         }
